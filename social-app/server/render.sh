7dd16c3e872a5300159b64d893fdd310691569e1
Kihog188 <khiemdora@gmail.com>
2026-03-21 21:47:24 +0700

    fix

--- social-app/server/render.sh
+++ social-app/server/render.sh
@@ -11,10 +11,9 @@ from apps.users.models import User
 email = os.environ.get('ADMIN_EMAIL', '')
 username = os.environ.get('ADMIN_USERNAME', '')
 password = os.environ.get('ADMIN_PASSWORD', '')
-if email and password and not User.objects.filter(is_superuser=True).exists():
+if email and username and password and not User.objects.filter(is_superuser=True).exists():
     User.objects.create_superuser(email=email, username=username, password=password)
     print("Superuser created!")
 else:
-    print("Skipped.")
+    print("Skipped superuser creation.")
 EOF
-```
