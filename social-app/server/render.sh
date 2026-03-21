73cef7bf113efe7bebb2e752854fb64e46abfded
Kihog188 <khiemdora@gmail.com>
2026-03-21 21:35:33 +0700

    ..

--- social-app/server/render.sh
+++ social-app/server/render.sh
@@ -4,3 +4,17 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
+
+python manage.py shell << 'EOF'
+import os
+from apps.users.models import User
+email = os.environ.get('ADMIN_EMAIL', '')
+username = os.environ.get('ADMIN_USERNAME', '')
+password = os.environ.get('ADMIN_PASSWORD', '')
+if email and password and not User.objects.filter(is_superuser=True).exists():
+    User.objects.create_superuser(email=email, username=username, password=password)
+    print("Superuser created!")
+else:
+    print("Skipped.")
+EOF
+```
